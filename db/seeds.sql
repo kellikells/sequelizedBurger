068c927ae5c96fbcfaea00db533656fdcbdea4fa
kelli <kellikells@gmail.com>
2020-04-03 18:51:54 -0700

    copied over eat-da-burger files

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('cheeseburger', 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ('double double', 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ('hamburger', 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ('chicken burger', 0);
